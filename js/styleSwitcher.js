a5a5c51e600fc0d62205e04357d7bd5c5415c34a
Abdallah <abdo.aa797@gmail.com>
2022-02-10 11:05:09 +0200

    add new loader

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,16 +1,17 @@
-if(localStorage.getItem("color") != "") {
-    let colorName = localStorage.getItem("color");
-    $(".alternate-style").attr("disabled", "true");
-    $(".alternate-style[title="+colorName+"]").removeAttr("disabled");
+if (localStorage.getItem("color") != "") {
+  let colorName = localStorage.getItem("color");
+  $(".alternate-style").attr("disabled", "true");
+  $(".alternate-style[title=" + colorName + "]").removeAttr("disabled");
 }
 
 function setActiveSyle(colorName) {
-    let chosenColor = colorName;
-    $(".alternate-style").attr("disabled", "true");
-    $(".alternate-style[title="+colorName+"]").removeAttr("disabled");
-     localStorage.setItem("color", chosenColor);
+  let chosenColor = colorName;
+  $(".alternate-style").attr("disabled", "true");
+  $(".alternate-style[title=" + colorName + "]").removeAttr("disabled");
+  localStorage.setItem("color", chosenColor);
 }
 
-$(".toggle-style-switcher").click(function () { 
-    $(".style-switcher").toggleClass("open")
+$(".toggle-style-switcher").click(function () {
+  $(".style-switcher").toggleClass("open");
 });
+
